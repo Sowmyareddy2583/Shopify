0ea9bdf1e3d83b7675aabcbbfdf3eec78ec20b4d
Sowmyareddy2583 <127490161+Sowmyareddy2583@users.noreply.github.com>
2024-06-29 13:52:24 +0530

    product page done

--- src/App.js
+++ src/App.js
@@ -7,9 +7,9 @@ import Product from "./Pages/Product";
 import Cart from "./Pages/Cart";
 import LoginSignUp from "./Pages/LoginSignUp";
 import Footer from "./Components/Footer/Footer";
-import men_banner from './Components/Assets/banner_mens.png'
-import women_banner from './Components/Assets/banner_women.png'
-import kids_banner from './Components/Assets/banner_kids.png'
+import men_banner from "./Components/Assets/banner_mens.png";
+import women_banner from "./Components/Assets/banner_women.png";
+import kids_banner from "./Components/Assets/banner_kids.png";
 function App() {
   return (
     <div>
@@ -17,17 +17,25 @@ function App() {
         <Navbar />
         <Routes>
           <Route path="/" element={<Shop />} />
-          <Route path="/mens" element={<ShopCategory  banner={men_banner} category="men" />} />
-          <Route path="/women" element={<ShopCategory banner={women_banner} category="women" />} />
-          <Route path="/kids" element={<ShopCategory banner={kids_banner}category="kid" />} />
+          <Route
+            path="/mens"
+            element={<ShopCategory banner={men_banner} category="men" />}
+          />
+          <Route
+            path="/women"
+            element={<ShopCategory banner={women_banner} category="women" />}
+          />
+          <Route
+            path="/kids"
+            element={<ShopCategory banner={kids_banner} category="kid" />}
+          />
           <Route path="/product" element={<Product />}>
             <Route path=":productId" element={<Product />} />
           </Route>
-          <Route path="/cart" element={<Cart />}/>
-          <Route path="/login" element={<LoginSignUp />}/>
-
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/login" element={<LoginSignUp />} />
         </Routes>
-        <Footer/>
+        <Footer />
       </BrowserRouter>
     </div>
   );
---
+++ src/Components/ProductDisplay/ProductDisplay.jsx
@@ -0,0 +1,58 @@
+import React from "react";
+import "./ProductDisplay.css";
+import star_icon from "../Assets/star_icon.png";
+import star_dull_icon from "../Assets/star_dull_icon.png";
+
+const ProductDisplay = (props) => {
+  const { product } = props;
+  return (
+    <div className="productdisplay">
+      <div className="product-left">
+        <div className="product-img-list">
+          <img src={product.image} alt="" />
+          <img src={product.image} alt="" />
+          <img src={product.image} alt="" />
+          <img src={product.image} alt="" />
+          
+        </div>
+        <div className="product-img">
+          <img className="product-img-main" src={product.image} alt="" />
+        </div>
+      </div>
+      <div className="product-right">
+        <h1>{product.name}</h1>
+        <div className="product-right-star">
+          <img src={star_icon} alt="" />
+          <img src={star_icon} alt="" />
+          <img src={star_icon} alt="" />
+          <img src={star_icon} alt="" />
+          <img src={star_dull_icon} alt="" />
+          <p>(122)</p>
+        </div>
+        <div className="product-prices">
+          <div className="product-oldprice">${product.old_price}</div>
+          <div className="product-newprice">${product.new_price}</div>
+        </div>
+        <div className="product-description">
+          Step into the spotlight with our Elegant Evening Gown, designed to
+          make you feel like a star at any event. 
+        </div>
+        <div className="product-size">
+            <h1>Select Size</h1>
+            <div className="display-size">
+                <div>S</div>
+                <div>M</div>
+                <div>L</div>
+                <div>XL</div>
+                <div>XXL</div>
+            </div>
+        </div>
+        <button>ADD TO CART</button>
+        <div className="product-category"><span>Category : </span> women,T-shirt,crop Top</div>
+        <div className="product-category"><span>Tags : </span>Latest, Modern </div>
+      </div>
+    </div>
+  );
+};
+
+export default ProductDisplay;
